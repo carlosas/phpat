259ea05c1e3eba3efae4facdf6a582d7069d5fad
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2019-08-03 19:41:02 +0200

    Update filter iterator to work with asterisks

--- src/File/PathnameFilterIterator.php
+++ src/File/PathnameFilterIterator.php
@@ -4,15 +4,26 @@ namespace PHPArchiTest\File;
 
 use Symfony\Component\Finder\Iterator\PathFilterIterator;
 
-class PathNameFilterIterator extends PathFilterIterator
+class PathnameFilterIterator extends PathFilterIterator
 {
     public function accept(): bool
     {
         $filename = $this->current()->getPathname();
+
         if ('\\' === \DIRECTORY_SEPARATOR) {
             $filename = str_replace('\\', '/', $filename);
         }
 
         return $this->isAccepted($filename);
+    }
+
+    protected function toRegex($str)
+    {
+        return $this->isRegex($str) ? $str : str_replace('\*', '(.)*', ('/'.preg_quote($str, '/').'/'));
+    }
+
+    protected function isRegex($str): bool
+    {
+        return false !== strpos($str, '*');
     }
 }
