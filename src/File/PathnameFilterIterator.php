c3ee81c0d7ec01488aaf7c83650506dd01b1f703
Carlos A. Sastre <carlos.alandete@gmail.com>
2019-08-07 18:07:17 +0200

    Fix filter iterator to handle asterisks

--- src/File/PathnameFilterIterator.php
+++ src/File/PathnameFilterIterator.php
@@ -19,11 +19,34 @@ class PathnameFilterIterator extends MultiplePcreFilterIterator
 
     protected function toRegex($str)
     {
-        return $this->isRegex($str) ? $str : str_replace('\*', '(.)*', ('/'.preg_quote($str, '/').'/'));
+        $r = $this->isRegex($str) ? $this->parseRegex($str) : preg_quote($str, '/');
+
+        return '/'.$r.'/';
     }
 
     protected function isRegex($str): bool
     {
         return false !== strpos($str, '*');
+    }
+
+    protected function parseRegex(string $str)
+    {
+        $substrs = explode('*', $str);
+        if (1 === count($substrs)) {
+            return $str;
+        }
+
+        $result = '';
+        foreach ($substrs as $k => $s) {
+            $s = str_replace('/', '\/', $s);
+            if ($k === 0) {
+                $result .= '('.$s.')';
+                continue;
+            }
+
+            $result .= '(\w)*('.$s.')';
+        }
+
+        return $result;
     }
 }
