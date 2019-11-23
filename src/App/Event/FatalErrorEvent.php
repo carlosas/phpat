60cc31e80a347f8245662e78c51e9b34e777bdec
Carlos A. Sastre <carlos.alandete@gmail.com>
2019-11-23 13:30:32 +0100

    Add basic warning and fatal error events

---
+++ src/App/Event/FatalErrorEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpAT\App\Event;
+
+use PhpAT\App\Event;
+
+class FatalErrorEvent extends Event
+{
+    /**
+     * @var string
+     */
+    private $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
