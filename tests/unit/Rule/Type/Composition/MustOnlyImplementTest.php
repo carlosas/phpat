4c5be5ad1cf29002e5378566c4f49796a1ea19b3
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2019-12-30 22:15:40 +0100

    Add MustOnlyImplement unit test

---
+++ tests/unit/Rule/Type/Composition/MustOnlyImplementTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\PhpAT\unit\Rule\Type\Composition;
+
+use PHPAT\EventDispatcher\EventDispatcher;
+use PhpAT\Parser\AstNode;
+use PhpAT\Parser\ClassName;
+use PhpAT\Rule\Type\Composition\CanOnlyImplement;
+use PhpAT\Rule\Type\Composition\MustImplement;
+use PhpAT\Rule\Type\Composition\MustOnlyImplement;
+use PhpAT\Statement\Event\StatementNotValidEvent;
+use PhpAT\Statement\Event\StatementValidEvent;
+use PHPUnit\Framework\TestCase;
+
+class MustOnlyImplementTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     * @param string $fqcnOrigin
+     * @param array  $fqcnDestinations
+     * @param array  $astMap
+     * @param bool   $inverse
+     * @param array  $expectedEvents
+     */
+    public function testDispatchesCorrectEvents(
+        string $fqcnOrigin,
+        array $fqcnDestinations,
+        array $astMap,
+        bool $inverse,
+        array $expectedEvents
+    ): void
+    {
+        $eventDispatcherMock = $this->createMock(EventDispatcher::class);
+        $class = new MustOnlyImplement($eventDispatcherMock);
+
+        foreach ($expectedEvents as $valid) {
+            $eventType = $valid ? StatementValidEvent::class : StatementNotValidEvent::class;
+            $consecutive[] = [$this->isInstanceOf($eventType)];
+        }
+
+        $eventDispatcherMock
+            ->expects($this->exactly(count($consecutive)))
+            ->method('dispatch')
+            ->withConsecutive(...$consecutive);
+
+        $class->validate($fqcnOrigin, $fqcnDestinations, $astMap, $inverse);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [
+                'Example\ClassExample',
+                ['Example\InterfaceExample', 'Example\AnotherInterface'],
+                $this->getAstMap(),
+                false,
+                [true, true, true]
+            ],
+            [
+                'Example\ClassExample',
+                ['Example\InterfaceExample', 'Example\AnotherInterface', 'NotImplementedInterface'],
+                $this->getAstMap(),
+                false,
+                [true, true, false, true]
+            ],
+            //it fails because Example\AnotherInterface is also implemented
+            ['Example\ClassExample', ['Example\InterfaceExample'], $this->getAstMap(), false, [true, false]],
+            //it fails because there are 2 interface implementations not listed
+            ['Example\ClassExample', ['NotARealInterface'], $this->getAstMap(), false, [false, false, false]],
+        ];
+    }
+
+    private function getAstMap(): array
+    {
+        return [
+            new AstNode(
+                new \SplFileInfo('folder/Example/ClassExample.php'), //File
+                new ClassName('Example', 'ClassExample'), //Classname
+                new ClassName('Example', 'ParentClassExample'), //Parent
+                [
+                    new ClassName('Example', 'AnotherClassExample'), //Dependency
+                    new ClassName('Vendor', 'ThirdPartyExample') //Dependency
+                ],
+                [
+                    new ClassName('Example', 'InterfaceExample'), //Interface
+                    new ClassName('Example', 'AnotherInterface') //Interface
+                ],
+                [new ClassName('Example', 'TraitExample')] //Trait
+            )
+        ];
+    }
+}
