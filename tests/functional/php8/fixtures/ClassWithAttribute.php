e1a41f1af69093e6238507acfe2e6c7df817c3fb
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2021-10-01 00:41:40 +0200

    Use a constant for an attribute construction

--- tests/functional/php8/fixtures/ClassWithAttribute.php
+++ tests/functional/php8/fixtures/ClassWithAttribute.php
@@ -5,7 +5,7 @@ namespace Tests\PhpAT\functional\php8\fixtures;
 #[DummyAttributeOne]
 class ClassWithAttribute
 {
-    #[DummyAttributeTwo, DummyAttributeThree('answer', 42)]
+    #[DummyAttributeTwo, DummyAttributeThree('answer', DummyAttributeTwo::SOME_INTEGER)]
     public function someMethod(): void
     {
     }
--- tests/functional/php8/fixtures/DummyAttributeTwo.php
+++ tests/functional/php8/fixtures/DummyAttributeTwo.php
@@ -4,4 +4,5 @@ namespace Tests\PhpAT\functional\php8\fixtures;
 
 #[\Attribute(\Attribute::TARGET_METHOD)] class DummyAttributeTwo
 {
+    public const SOME_INTEGER = 42;
 }
