47fcf19f56f9c920839c02f9a736ec37c33ac915
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2021-12-25 22:41:32 +0100

    basic bench with 3 configs

--- tests/benchmark/AppExecutionBench.php
+++ tests/benchmark/AppExecutionBench.php
@@ -7,8 +7,22 @@ use Symfony\Component\Console\Output\NullOutput;
 
 class AppExecutionBench
 {
-    public function benchRun()
+    /**
+     * @Iterations(10)
+     * @Sleep(1000)
+     * @ParamProviders({"provideConfigPaths"})
+     */
+    public function benchRun($params)
     {
-        (new \PhpAT\App())->run(new ArrayInput([]), new NullOutput());
+        (new \PhpAT\App())->run(new ArrayInput(['config' => realpath($params['path'])]), new NullOutput());
+    }
+
+    public function provideConfigPaths(): array
+    {
+        return [
+            'base' => ['path' => __DIR__ . '/../../ci/phpat.yaml'],
+            'functional7' => ['path' => __DIR__ . '/../../tests/functional/functional7.yaml'],
+            'functional8' => ['path' => __DIR__ . '/../../tests/functional/functional8.yaml'],
+        ];
     }
 }
