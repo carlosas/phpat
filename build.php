cc6f16f8ae5dd58704f1a913ce87564c82291591
dabrowiecki <dabrowiecki@users.noreply.github.com>
2022-04-22 17:36:00 +0100

    Fix broken phar file

--- build.php
+++ build.php
@@ -6,7 +6,7 @@ $compiler = new PhpAT\Compiler([
     'base'   => __DIR__,
     'src'    => __DIR__ . DIRECTORY_SEPARATOR . 'src',
     'vendor' => __DIR__ . DIRECTORY_SEPARATOR . 'vendor',
-    'binary' => __DIR__ . DIRECTORY_SEPARATOR . 'phpat',
+    'binary' => __DIR__ . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'phpat',
     'output' => __DIR__ . DIRECTORY_SEPARATOR . 'dist',
 ]);
 
