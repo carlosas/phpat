e994dfe996bd7c074d5f6b2c2d1c0836b78023e3
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2022-05-20 20:50:40 +0200

    Add first tests

---
+++ tests/architecture/ConfigurationTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\PhpAT\architecture;
+
+use PhpAT\ArchitectureTest;
+use PhpAT\Configuration;
+use PhpAT\Selector;
+use PhpAT\Test\Rule;
+
+class ConfigurationTest extends ArchitectureTest
+{
+    public function test_configuration_does_not_depend_on_rules(): Rule
+    {
+        return $this->rule()
+            ->classes(Selector::classname(Configuration::class))
+            ->mustNotDependOn()
+            ->classes(Selector::classname(Rule::class)) //change to all namespace
+            ->build();
+    }
+}
---
+++ tests/architecture/FailingTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\PhpAT\architecture;
+
+use PhpAT\ArchitectureTest;
+use PhpAT\DumbShit;
+use PhpAT\Selector;
+use PhpAT\SimpleClass;
+use PhpAT\SomeInterface;
+use PhpAT\Test\Rule;
+
+class FailingTest extends ArchitectureTest
+{
+    public function test_configuration_does_not_depend_on_rules(): Rule
+    {
+        return $this->rule()
+            ->classes(Selector::classname(SimpleClass::class))
+            ->mustNotDependOn()
+            ->classes(
+                Selector::classname(DumbShit::class),
+                Selector::implements(SomeInterface::class)
+            )
+            ->build();
+    }
+}
