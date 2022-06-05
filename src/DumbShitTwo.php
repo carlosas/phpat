5a023c48f2dbf4776fa15cfc2e960e7de9b73e70
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2022-06-05 17:00:06 +0200

    Remove test code

--- src/DumbShitTwo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PHPat;
-
-class DumbShitTwo implements SomeInterface
-{
-}
--- src/SomeAbstractClass.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PHPat;
-
-abstract class SomeAbstractClass
-{
-}
