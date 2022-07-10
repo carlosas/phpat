0d752895fce5bd9df99cd6ead17e9306a0b0f2ce
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2022-07-10 12:52:12 +0200

    Add should not implement unit test

---
+++ tests/unit/rules/ShouldNotImplement/ImplementedInterfacesTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\PHPat\unit\rules\ShouldNotImplement;
+
+use PHPat\Rule\Assertion\ShouldNotImplement\ImplementedInterfacesRule;
+use PHPat\Rule\Assertion\ShouldNotImplement\ShouldNotImplement;
+use PHPat\Selector\Classname;
+use PHPat\Statement\Builder\StatementBuilderFactory;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use PHPStan\Type\FileTypeMapper;
+use Tests\PHPat\fixtures\FixtureClass;
+use Tests\PHPat\fixtures\Simple\SimpleInterface;
+use Tests\PHPat\unit\FakeTestParser;
+
+/**
+ * @extends RuleTestCase<ImplementedInterfacesRule>
+ */
+class ImplementedInterfacesTest extends RuleTestCase
+{
+    public function testRule(): void
+    {
+        $this->analyse(['tests/fixtures/FixtureClass.php'], [
+            [sprintf('%s should not implement %s', FixtureClass::class, SimpleInterface::class), 28],
+        ]);
+    }
+
+    protected function getRule(): Rule
+    {
+        $testParser = FakeTestParser::create(
+            ShouldNotImplement::class,
+            [new Classname(FixtureClass::class)],
+            [new Classname(SimpleInterface::class)]
+        );
+
+        return new ImplementedInterfacesRule(
+            new StatementBuilderFactory($testParser),
+            $this->createReflectionProvider(),
+            self::getContainer()->getByType(FileTypeMapper::class)
+        );
+    }
+}
