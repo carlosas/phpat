ca7d5bfa6826c6e339c8f62c917020ad12d22333
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2023-01-28 12:36:50 +0100

    add selector

---
+++ src/Selector/IsAttribute.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPat\Selector;
+
+use PHPStan\Reflection\ClassReflection;
+
+class IsAttribute implements SelectorInterface
+{
+    public function getName(): string
+    {
+        return '-attribute classes-';
+    }
+
+    public function matches(ClassReflection $classReflection): bool
+    {
+        return $classReflection->isAttributeClass();
+    }
+}
