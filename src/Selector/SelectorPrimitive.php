77a02cf6838bdd7edcbf16cc4f054d07e05c6bec
Carlos A Sastre <carlos.alandete@gmail.com>
2023-08-14 17:44:50 +0200

    Ignore psalm non-empty-string warning

--- src/Selector/SelectorPrimitive.php
+++ src/Selector/SelectorPrimitive.php
@@ -52,21 +52,33 @@ class SelectorPrimitive
         return new IsAttribute();
     }
 
+    /**
+     * @param class-string|non-empty-string $namespace
+     */
     public static function namespace(string $namespace, bool $regex = false): ClassNamespace
     {
         return new ClassNamespace($namespace, $regex);
     }
 
+    /**
+     * @param class-string|non-empty-string $fqcn
+     */
     public static function classname(string $fqcn, bool $regex = false): Classname
     {
         return new Classname($fqcn, $regex);
     }
 
+    /**
+     * @param class-string|non-empty-string $fqcn
+     */
     public static function implements(string $fqcn, bool $regex = false): ClassImplements
     {
         return new ClassImplements($fqcn, $regex);
     }
 
+    /**
+     * @param class-string|non-empty-string $fqcn
+     */
     public static function extends(string $fqcn, bool $regex = false): ClassExtends
     {
         return new ClassExtends($fqcn, $regex);
