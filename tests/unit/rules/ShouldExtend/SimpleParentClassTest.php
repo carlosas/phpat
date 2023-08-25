7cb9d063b42ec3b37cedc2eb714bc1aac1ca8cbd
Carlos A Sastre <carlos.alandete@gmail.com>
2023-08-25 10:38:42 +0200

    Test fix for Should rules ignoring classes with empty findings

---
+++ tests/unit/rules/ShouldExtend/SimpleParentClassTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace Tests\PHPat\unit\rules\ShouldExtend;
+
+use PHPat\Configuration;
+use PHPat\Rule\Assertion\Relation\ShouldExtend\ParentClassRule;
+use PHPat\Rule\Assertion\Relation\ShouldExtend\ShouldExtend;
+use PHPat\Selector\Classname;
+use PHPat\Statement\Builder\StatementBuilderFactory;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use PHPStan\Type\FileTypeMapper;
+use Tests\PHPat\fixtures\FixtureClass;
+use Tests\PHPat\fixtures\Simple\SimpleAbstractClass;
+use Tests\PHPat\fixtures\Simple\SimpleAbstractClassTwo;
+use Tests\PHPat\fixtures\Simple\SimpleClass;
+use Tests\PHPat\unit\FakeTestParser;
+
+/**
+ * @extends RuleTestCase<ParentClassRule>
+ * @internal
+ * @coversNothing
+ */
+class SimpleParentClassTest extends RuleTestCase
+{
+    public const RULE_NAME = 'test_SimpleClassShouldExtendSimpleAbstractClass';
+
+    public function testRule(): void
+    {
+        $this->analyse(['tests/fixtures/Simple/SimpleClass.php'], [
+            [sprintf('%s should extend %s', SimpleClass::class, SimpleAbstractClass::class), 5],
+        ]);
+    }
+
+    protected function getRule(): Rule
+    {
+        $testParser = FakeTestParser::create(
+            self::RULE_NAME,
+            ShouldExtend::class,
+            [new Classname(SimpleClass::class, false)],
+            [new Classname(SimpleAbstractClass::class, false)]
+        );
+
+        return new ParentClassRule(
+            new StatementBuilderFactory($testParser),
+            new Configuration(false, true, false),
+            $this->createReflectionProvider(),
+            self::getContainer()->getByType(FileTypeMapper::class)
+        );
+    }
+}
