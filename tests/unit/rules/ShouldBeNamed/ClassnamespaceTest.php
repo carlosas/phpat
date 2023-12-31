546f5bb9a7a558534e6532e34bdf336ac838a97c
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2023-12-31 14:40:08 +0700

    Added a test

---
+++ tests/unit/rules/ShouldBeNamed/ClassnamespaceTest.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace Tests\PHPat\unit\rules\ShouldBeNamed;
+
+use PHPat\Configuration;
+use PHPat\Rule\Assertion\Declaration\ShouldBeNamed\ClassnameRule;
+use PHPat\Rule\Assertion\Declaration\ShouldBeNamed\ShouldBeNamed;
+use PHPat\Selector\Classname;
+use PHPat\Selector\ClassNamespace;
+use PHPat\Statement\Builder\StatementBuilderFactory;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use PHPStan\Type\FileTypeMapper;
+use Tests\PHPat\fixtures\FixtureClass;
+use Tests\PHPat\fixtures\Namespace\Foo\ClassUnderFooNamespace;
+use Tests\PHPat\fixtures\Namespace\FooBar\ClassUnderFooBarNamespace;
+use Tests\PHPat\unit\FakeTestParser;
+
+/**
+ * @extends RuleTestCase<ClassnameRule>
+ * @internal
+ * @coversNothing
+ */
+class ClassnamespaceTest extends RuleTestCase
+{
+    public const RULE_NAME = 'test_FixtureClassUnderNamespaceShouldBeNamed';
+
+    public function testRule(): void
+    {
+        // Class under FooBar should not subject to the rule
+        $this->analyse(['tests/fixtures/Namespace/FooBar/ClassUnderFooBarNamespace.php'], []);
+    }
+
+    protected function getRule(): Rule
+    {
+        $testParser = FakeTestParser::create(
+            self::RULE_NAME,
+            ShouldBeNamed::class,
+            [new ClassNamespace('Tests\PHPat\fixtures\Namespace\Foo', false)],
+            [],
+            [],
+            ['isRegex' => false, 'classname' => ClassUnderFooNamespace::class]
+        );
+
+        return new ClassnameRule(
+            new StatementBuilderFactory($testParser),
+            new Configuration(false, true, false),
+            $this->createReflectionProvider(),
+            self::getContainer()->getByType(FileTypeMapper::class)
+        );
+    }
+}
