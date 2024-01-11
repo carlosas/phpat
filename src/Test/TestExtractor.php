cc5d5b13d6918c279935d8f5ec6128d145e4bf08
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2024-01-11 15:37:30 +0100

    Adapt cs fixer for php 8

--- src/Test/TestExtractor.php
+++ src/Test/TestExtractor.php
@@ -26,7 +26,7 @@ final class TestExtractor implements TestExtractorInterface
                 throw new ShouldNotHappenException();
             }
 
-            $reflectedTest = $this->reflectTest(get_class($test));
+            $reflectedTest = $this->reflectTest($test::class);
             if ($reflectedTest !== null) {
                 yield $reflectedTest;
             }
