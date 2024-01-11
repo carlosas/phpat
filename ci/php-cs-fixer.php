ed1add2f1bcf83943348ed035f016c6e676d9ada
Carlos A Sastre <carlos.alandete@gmail.com>
2024-01-11 14:55:10 +0100

    Change minimum PHP version to 8.0 and introduce 8.3

--- ci/php-cs-fixer.php
+++ ci/php-cs-fixer.php
@@ -9,8 +9,8 @@ $finder = PhpCsFixer\Finder::create()
 
 $rules = [
     '@PER'                        => true,
+    '@PHP80Migration'             => true,
     '@PHP80Migration:risky'       => true,
-    '@PHP82Migration'             => true,
     '@PhpCsFixer'                 => true,
     PhpCsFixerCustomFixers\Fixer\DeclareAfterOpeningTagFixer::name() => true,
     PhpCsFixerCustomFixers\Fixer\PhpdocArrayStyleFixer::name() => true,
