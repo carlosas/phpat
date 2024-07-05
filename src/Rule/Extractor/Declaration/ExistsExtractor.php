24f657bcf694fe5bf9f6aef8945bae7fff15e4fe
Anna Damm <anna.damm87@googlemail.com>
2024-07-05 16:39:37 +0200

    Add shouldNotExist assertion

---
+++ src/Rule/Extractor/Declaration/ExistsExtractor.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace PHPat\Rule\Extractor\Declaration;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
+
+trait ExistsExtractor
+{
+    public function getNodeType(): string
+    {
+        return InClassNode::class;
+    }
+
+    /**
+     * @param InClassNode $node
+     */
+    protected function meetsDeclaration(Node $node, Scope $scope, array $params = []): bool
+    {
+        return true;
+    }
+}
