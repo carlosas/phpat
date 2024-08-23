e8f7c2961e362dea8e140f1cf2156e0a4c763285
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-23 14:52:15 -0500

    Create AtLeastModifier.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Selector/Modifier/AtLeastModifier.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace PHPat\Selector\Modifier;
+
+use PHPat\Selector\SelectorInterface;
+use PHPStan\Reflection\ClassReflection;
+
+final class AtLeastModifier implements SelectorInterface
+{
+    private int $min;
+    private array $selectors;
+
+    public function __construct(int $min, SelectorInterface ...$selector)
+    {
+        $this->min = $min;
+        $this->selectors = array_values($selector);
+    }
+
+    public function matches(ClassReflection $classReflection): bool
+    {
+        $matches = 0;
+
+        foreach ($this->selectors as $selector) {
+            if ($selector->matches($classReflection)) {
+                $matches++;
+            }
+        }
+
+        return $matches >= $this->min;
+    }
+
+    public function getName(): string
+    {
+        return 'at-least-' . $this->min . '-of: ' .  implode(
+            ', ',
+            array_map(static fn (SelectorInterface $selector) => $selector->getName(), $this->selectors),
+        );
+    }
+}
+
