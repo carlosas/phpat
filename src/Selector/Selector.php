216761491de1c36e28f4d94e4e5bdd362603ae9e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-01 13:06:45 -0500

    Update Selector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Selector/Selector.php
+++ src/Selector/Selector.php
@@ -2,8 +2,14 @@
 
 namespace PHPat\Selector;
 
+use PHPat\Selector\Modifier\AllOfSelectorModifier;
 use PHPat\Selector\Modifier\AndModifier;
+use PHPat\Selector\Modifier\AnyOfSelectorModifier;
+use PHPat\Selector\Modifier\AtLeastCountOfSelectorModifier;
+use PHPat\Selector\Modifier\AtMostCountOfSelectorModifier;
+use PHPat\Selector\Modifier\NoneOfSelectorModifier;
 use PHPat\Selector\Modifier\NotModifier;
+use PHPat\Selector\Modifier\OneOfSelectorModifier;
 
 final class Selector extends SelectorPrimitive
 {
@@ -15,5 +21,35 @@ final class Selector extends SelectorPrimitive
     public static function NOT(SelectorInterface $selector): NotModifier
     {
         return new NotModifier($selector);
+    }
+
+    public static function AllOf(SelectorInterface ...$selectors): AllOfSelectorModifier
+    {
+        return new AllOfSelectorModifier(...$selectors);
+    }
+
+    public static function AnyOf(SelectorInterface ...$selectors): AnyOfSelectorModifier
+    {
+        return new AnyOfSelectorModifier(...$selectors);
+    }
+
+    public static function NoneOf(SelectorInterface ...$selectors): NoneOfSelectorModifier
+    {
+        return new NoneOfSelectorModifier(...$selectors);
+    }
+
+    public static function AtLeastCountOf(int $count, SelectorInterface ...$selectors): AtLeastCountOfSelectorModifier
+    {
+        return new AtLeastCountOfSelectorModifier($count, ...$selectors);
+    }
+
+    public static function AtMostCountOf(int $count, SelectorInterface ...$selectors): AtMostCountOfSelectorModifier
+    {
+        return new AtMostCountOfSelectorModifier($count, ...$selectors);
+    }
+
+    public static function OneOf(SelectorInterface ...$selectors): OneOfSelectorModifier
+    {
+        return new OneOfSelectorModifier(...$selectors);
     }
 }
