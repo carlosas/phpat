fd4dc8407008ffc68bafd9ccbd181cac5f39abe8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-01 13:06:45 -0500

    Add AtLeastCountOfSelectorModifier.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Selector/Modifier/AtLeastCountOfSelectorModifier.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace PHPat\Selector\Modifier;
+
+use PHPat\Selector\SelectorInterface;
+use PHPStan\Reflection\ClassReflection;
+
+final class AtLeastCountOfSelectorModifier implements SelectorInterface
+{
+    private int $count;
+
+    /** @var array<SelectorInterface> */
+    private array $selectors;
+
+    public function __construct(int $count, SelectorInterface ...$selectors)
+    {
+        $this->count = $count;
+        $this->selectors = $selectors;
+    }
+
+    #[\Override]
+    public function getName(): string
+    {
+        return \sprintf(
+            'at least %d of %s',
+            $this->count,
+            \implode(' and ', \array_map(static fn ($selector) => $selector->getName(), $this->selectors)),
+        );
+    }
+
+    #[\Override]
+    public function matches(ClassReflection $classReflection): bool
+    {
+        $matches = 0;
+        foreach ($this->selectors as $selector) {
+            if ($selector->matches($classReflection)) {
+                ++$matches;
+            }
+
+            if ($matches >= $this->count) {
+                return true;
+            }
+        }
+
+        return $matches >= $this->count;
+    }
+}
