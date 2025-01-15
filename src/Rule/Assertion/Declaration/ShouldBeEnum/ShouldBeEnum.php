3943c0c5c5a4851ce5abcdf7c5c5cfceed8d2a60
naxvog <naxvog@users.noreply.github.com>
2025-01-16 00:56:03 +0100

    Add shouldBeEnum assertion

---
+++ src/Rule/Assertion/Declaration/ShouldBeEnum/ShouldBeEnum.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PHPat\Rule\Assertion\Declaration\ShouldBeEnum;
+
+use PHPat\Configuration;
+use PHPat\Rule\Assertion\Declaration\DeclarationAssertion;
+use PHPat\Rule\Assertion\Declaration\ValidationTrait;
+use PHPat\ShouldNotHappenException;
+use PHPat\Statement\Builder\StatementBuilderFactory;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Rules\RuleError;
+use PHPStan\Type\FileTypeMapper;
+
+abstract class ShouldBeEnum extends DeclarationAssertion
+{
+    use ValidationTrait;
+
+    public function __construct(
+        StatementBuilderFactory $statementBuilderFactory,
+        Configuration $configuration,
+        ReflectionProvider $reflectionProvider,
+        FileTypeMapper $fileTypeMapper
+    ) {
+        parent::__construct(
+            __CLASS__,
+            $statementBuilderFactory,
+            $configuration,
+            $reflectionProvider,
+            $fileTypeMapper
+        );
+    }
+
+    /**
+     * @param  array<string>            $tips
+     * @return array<RuleError>
+     * @throws ShouldNotHappenException
+     */
+    protected function applyValidation(string $ruleName, ClassReflection $subject, bool $meetsDeclaration, array $tips, array $params = []): array
+    {
+        return $this->applyShould($ruleName, $subject, $meetsDeclaration, $tips, $params);
+    }
+
+    protected function getMessage(string $ruleName, string $subject, array $params = []): string
+    {
+        return $this->prepareMessage(
+            $ruleName,
+            sprintf('%s should be enum', $subject)
+        );
+    }
+}
