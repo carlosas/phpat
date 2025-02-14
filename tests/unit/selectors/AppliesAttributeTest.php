b75b60a0e82289efc185e2d8409c9f1b9a80c057
Raffaele Carelle <raffaele.carelle@algoritma.it>
2025-02-14 14:29:40 +0100

    Refactor test annotations to use a data provider.
    Replaced PHPUnit attributes with a data provider in `AppliesAttributeTest`. This improves readability and centralizes

--- tests/unit/selectors/AppliesAttributeTest.php
+++ tests/unit/selectors/AppliesAttributeTest.php
@@ -4,8 +4,6 @@ namespace Tests\PHPat\unit\selectors;
 
 use PHPat\Selector\AppliesAttribute;
 use PHPStan\Reflection\ClassReflection;
-use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
 use Tests\PHPat\fixtures\FixtureClass;
 use Tests\PHPat\fixtures\Simple\SimpleAttribute;
@@ -15,7 +13,6 @@ use Tests\PHPat\fixtures\Simple\SimpleAttribute;
  *
  * @covers \PHPat\Selector\AppliesAttribute
  */
-#[CoversClass(AppliesAttribute::class)]
 class AppliesAttributeTest extends TestCase
 {
     protected function setUp(): void
@@ -27,31 +24,43 @@ class AppliesAttributeTest extends TestCase
         }
     }
 
-    public function testGetName()
+    public function testGetName(): void
     {
         $selector = new AppliesAttribute('test');
 
         $this->assertEquals('test', $selector->getName());
     }
 
-    #[TestWith([SimpleAttribute::class, [], false, true])]
-    #[TestWith(['Fake\Attribute', [], false, false])]
-    #[TestWith(['/\bSimple[A-Z][a-zA-Z0-9_]*\b/', [], true, true])]
-    #[TestWith(['/\bFake[A-Z][a-zA-Z0-9_]*\b/', [], true, false])]
-    #[TestWith([SimpleAttribute::class, ['something' => 'something'], false, true])]
-    #[TestWith([SimpleAttribute::class, ['something' => 'somethingElse'], false, false])]
-    public function testMatches(string $name, array $arguments, bool $isRegex, bool $expected)
+    /**
+     * @dataProvider getCases
+     */
+    public function testMatches(string $name, array $arguments, bool $isRegex, bool $expected): void
     {
         $selector = new AppliesAttribute($name, $isRegex, $arguments);
 
         self::assertSame($expected, $selector->matches($this->getClassReflection()));
     }
 
+    public function getCases(): array
+    {
+        return [
+            [SimpleAttribute::class, [], false, true],
+            ['Fake\Attribute', [], false, false],
+            ['/\bSimple[A-Z][a-zA-Z0-9_]*\b/', [], true, true],
+            ['/\bFake[A-Z][a-zA-Z0-9_]*\b/', [], true, false],
+            [SimpleAttribute::class, ['something' => 'something'], false, true],
+            [SimpleAttribute::class, ['something' => 'somethingElse'], false, false],
+        ];
+    }
+
     private function getClassReflection(): ClassReflection
     {
         $ref = new \ReflectionClass(ClassReflection::class);
         $instance = $ref->newInstanceWithoutConstructor();
-        $ref->getProperty('reflection')->setValue($instance, new \ReflectionClass(FixtureClass::class));
+
+        $reflectionProperty = $ref->getProperty('reflection');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($instance, new \ReflectionClass(FixtureClass::class));
 
         return $instance;
     }
