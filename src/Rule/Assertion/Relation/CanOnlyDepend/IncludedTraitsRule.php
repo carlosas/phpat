1be93ef8c6539bb7a662d843cb03be9e6c459cb1
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2025-02-14 10:59:55 +0100

    Catch included traits in dependency assertions

---
+++ src/Rule/Assertion/Relation/CanOnlyDepend/IncludedTraitsRule.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace PHPat\Rule\Assertion\Relation\CanOnlyDepend;
+
+use PHPat\Rule\Assertion\Relation\ShouldInclude\ShouldInclude;
+use PHPat\Rule\Extractor\Relation\AllTraitsExtractor;
+use PHPStan\Node\InClassNode;
+use PHPStan\Rules\Rule;
+
+/**
+ * @implements Rule<InClassNode>
+ */
+final class IncludedTraitsRule extends CanOnlyDepend implements Rule
+{
+    use AllTraitsExtractor;
+}
---
+++ tests/unit/rules/CanOnlyDepend/IncludedTraitsTest.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace Tests\PHPat\unit\rules\CanOnlyDepend;
+
+use PHPat\Configuration;
+use PHPat\Rule\Assertion\Relation\CanOnlyDepend\CanOnlyDepend;
+use PHPat\Rule\Assertion\Relation\CanOnlyDepend\IncludedTraitsRule;
+use PHPat\Selector\Classname;
+use PHPat\Statement\Builder\StatementBuilderFactory;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use PHPStan\Type\FileTypeMapper;
+use Tests\PHPat\fixtures\FixtureClass;
+use Tests\PHPat\fixtures\Simple\SimpleClass;
+use Tests\PHPat\fixtures\Simple\SimpleTrait;
+use Tests\PHPat\unit\FakeTestParser;
+
+/**
+ * @extends RuleTestCase<IncludedTraitsRule>
+ * @internal
+ * @coversNothing
+ */
+class IncludedTraitsTest extends RuleTestCase
+{
+    public const RULE_NAME = 'testFixtureClassCanOnlyDependSimpleException';
+
+    public function testRule(): void
+    {
+        $this->analyse(['tests/fixtures/FixtureClass.php'], [
+            [sprintf('%s should not depend on %s', FixtureClass::class, SimpleTrait::class), 29],
+        ]);
+    }
+
+    protected function getRule(): Rule
+    {
+        $testParser = FakeTestParser::create(
+            self::RULE_NAME,
+            CanOnlyDepend::class,
+            [new Classname(FixtureClass::class, false)],
+            [new Classname(SimpleClass::class, false)]
+        );
+
+        return new IncludedTraitsRule(
+            new StatementBuilderFactory($testParser),
+            new Configuration(false, true, false),
+            $this->createReflectionProvider(),
+            self::getContainer()->getByType(FileTypeMapper::class)
+        );
+    }
+}
