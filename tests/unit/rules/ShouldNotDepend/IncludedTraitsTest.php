dd8be7ec1397ad762993e7bb2caa7b12792a7791
Carlos A Sastre <carlos.alandete@gmail.com>
2025-02-14 12:04:05 +0100

    Catch included traits in dependency assertions

---
+++ tests/unit/rules/ShouldNotDepend/IncludedTraitsTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Tests\PHPat\unit\rules\ShouldNotDepend;
+
+use PHPat\Configuration;
+use PHPat\Rule\Assertion\Relation\ShouldNotDepend\IncludedTraitsRule;
+use PHPat\Rule\Assertion\Relation\ShouldNotDepend\ShouldNotDepend;
+use PHPat\Selector\Classname;
+use PHPat\Statement\Builder\StatementBuilderFactory;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use PHPStan\Type\FileTypeMapper;
+use Tests\PHPat\fixtures\FixtureClass;
+use Tests\PHPat\fixtures\Simple\SimpleTrait;
+use Tests\PHPat\unit\FakeTestParser;
+
+/**
+ * @extends RuleTestCase<IncludedTraitsRule>
+ * @internal
+ * @coversNothing
+ */
+class IncludedTraitsTest extends RuleTestCase
+{
+    public const RULE_NAME = 'testFixtureClassShouldNotDependSimpleAndSpecial';
+
+    public function testRule(): void
+    {
+        $this->analyse(['tests/fixtures/FixtureClass.php'], [
+            [sprintf('%s should not depend on %s', FixtureClass::class, SimpleTrait::class), 29],
+        ]);
+    }
+
+    protected function getRule(): Rule
+    {
+        $testParser = FakeTestParser::create(
+            self::RULE_NAME,
+            ShouldNotDepend::class,
+            [new Classname(FixtureClass::class, false)],
+            [new Classname(SimpleTrait::class, false)]
+        );
+
+        return new IncludedTraitsRule(
+            new StatementBuilderFactory($testParser),
+            new Configuration(false, true, false),
+            $this->createReflectionProvider(),
+            self::getContainer()->getByType(FileTypeMapper::class)
+        );
+    }
+}
