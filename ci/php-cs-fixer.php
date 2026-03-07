a19f76f3e7ca37b8fb3bac3f6460ae80f3f0cb38
Carlos Alandete Sastre <carlos.alandete@gmail.com>
2026-03-07 14:31:43 +0100

    Fix cs

--- ci/php-cs-fixer.php
+++ ci/php-cs-fixer.php
@@ -6,7 +6,9 @@ $finder = PhpCsFixer\Finder::create()
         dirname(__DIR__) . '/tests/architecture',
         dirname(__DIR__) . '/tests/unit',
     ])
-    ->notPath('unit/selectors/ReadonlyFixture.php');
+    ->exclude([
+        'unit/selectors/ReadonlyFixture.php',
+    ]);
 
 $rules = [
     '@PER-CS' => true,
